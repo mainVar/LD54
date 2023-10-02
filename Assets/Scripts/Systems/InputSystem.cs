957d644f7213367a894b729c54bb5a32703bd161
Alex Wargon <https://github.com/AlexWargon.png>
2023-10-03 00:25:35 +0300

    fix input

--- Assets/Scripts/Systems/InputSystem.cs
+++ Assets/Scripts/Systems/InputSystem.cs
@@ -6,10 +6,9 @@ namespace LD54 {
     partial class InputSystem : UpdateSystem {
         public override void Update() {
             entities.Without<Inactive>().Each((InputComponent input) => {
-                var h = Input.GetAxis("Horizontal");
-                var v = Input.GetAxis("Vertical");
-                input.horizontal = h > 0 ? 1 : h < 0 ? -1 : 0;
-                input.vertical =  v > 0 ? 1 : v < 0 ? -1 : 0;
+
+                input.horizontal = Input.GetKey(KeyCode.A) ? -1 : Input.GetKey(KeyCode.D) ? 1 : 0F;
+                input.vertical =  Input.GetKey(KeyCode.S) ? -1 : Input.GetKey(KeyCode.W) ? 1 : 0F;
                 input.run = new Vector2(input.horizontal, input.vertical).magnitude > 0.1f;
             });
         }
