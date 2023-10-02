9409d39c223913826b7db7b1818552d0ad209262
Vladyslav.Panchenko <>
2023-10-03 00:15:17 +0300

    [UPD] fix bug for build + re add sound

--- Assets/Scripts/Systems/PlayerDeathSystem.cs
+++ Assets/Scripts/Systems/PlayerDeathSystem.cs
@@ -5,7 +5,7 @@ namespace LD54 {
     partial class PlayerDeathSystem : UpdateSystem {
         public override void Update() {
             entities.Each((Entity entity, DeathEvent deathEvent, Player enemy, TransformRef transformRef) => {
-                Wargon.ezs.Unity.Log.Show(Color.red, "Game Over");
+                //Wargon.ezs.Unity.Log.Show(Color.red, "Game Over");
                 entity.Remove<DeathEvent>();
                 Object.Destroy(transformRef.value.gameObject);
             });
