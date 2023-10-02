9a31981faa50d98cf1359cc9fbea0b9a75dc6576
Alex Wargon <https://github.com/AlexWargon.png>
2023-10-02 23:15:42 +0300

    spawners and particles

--- Assets/Scripts/Systems/RoomSpawnerSystem.cs
+++ Assets/Scripts/Systems/RoomSpawnerSystem.cs
@@ -3,8 +3,8 @@ using Wargon.ezs.Unity;
 
 namespace LD54
 {
-    partial class RoomSpawnerSystem : UpdateSystem
-    {
+    partial class RoomSpawnerSystem : UpdateSystem {
+        private EnemySpawnersList oldSpawner;
         public override void Update()
         {
             entities.Without<Inactive>().Each((Entity entity, ActiveRoom activeRoom, EnableRoomSpawner enableRoomSpawner) =>
@@ -13,7 +13,15 @@ namespace LD54
                 {
                     spawnPoints.gameObject.SetActive(true);
                 }
-                
+
+                if (oldSpawner != null) {
+                    foreach (var spawnPoints in oldSpawner.EnemySpawners)
+                    {
+                        spawnPoints.gameObject.SetActive(false);
+                    }
+                }
+
+                oldSpawner = activeRoom.EnemySpawnersList;
                 entity.Remove<EnableRoomSpawner>();
                 entity.Add(new Inactive());
             });
